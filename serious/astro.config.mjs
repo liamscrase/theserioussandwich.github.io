7e2c7851828878277f96878a12647e50f1a7f31a
Liam Scrase <Liam.Scrase@avantifinance.co.nz>
2025-12-24 10:34:07 +1300

    Refactor code structure for improved readability and maintainability

--- serious/astro.config.mjs
+++ serious/astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://theserioussandwich.github.io',
+  site: 'https://theserioussandwich.nz',
   base: '/',
+  integrations: [sitemap()],
 });
